7d58ce967bc9aabf3031e8f3459011a86a50738f
Romina Espinosa <espinosasosaromina@gmail.com>
2024-08-14 23:16:03 -0300

    evito propagacion del evento click del boton al div

--- eventos.js
+++ eventos.js
@@ -1,4 +1,11 @@
+//eventos para el div//
 document.querySelector('div').addEventListener('click', function() {
     alert('Hola! soy el div');
 
+});
+
+//eventos para el botonn//
+document.querySelector('button').addEventListener('click', function(event) {
+    event.stopPropagation(); 
+    alert('Hola!');
 });
